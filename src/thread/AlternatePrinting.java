1c27bd430fe3db12379dcf783caec919f8aa2565
LiuZhuLan <1322468221@qq.com>
2020-10-05 17:56:17 +0800

    交替打印

---
+++ src/thread/AlternatePrinting.java
@@ -0,0 +1,38 @@
+package thread;
+
+/**
+ * 交替打印FooBar
+ * @author LiuZhulan
+ */
+public class AlternatePrinting {
+
+    private int n;
+
+    public AlternatePrinting(int n) {
+        this.n = n;
+    }
+
+    volatile boolean permitFoo = true;
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        for (int i = 0; i < n; ) {
+            if(permitFoo) {
+                printFoo.run();
+                i++;
+                permitFoo = false;
+            }
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        for (int i = 0; i < n; ) {
+            if(!permitFoo) {
+                printBar.run();
+                i++;
+                permitFoo = true;
+            }
+        }
+    }
+
+
+}
